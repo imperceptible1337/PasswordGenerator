0412d7f905cd156bf530dba06d62d245021c8f01
imperceptible <59235957+imperceptible1337@users.noreply.github.com>
2022-06-09 11:29:24 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,36 +2,34 @@
 from random import choice
 from colorama import Fore
 import colorama
-colorama.init()
+import argparse
 import random
 
-# continue generating a new password
-while True:
-    # the characters that can be used in the password
-    ALL_CHARACTERS = 'abcdefghijklmnopqsrtuvqxyzABCDEFGHIJKLMNOPQSRTUVQXYZ1234567890!@#$%^&*()-_+={}[]|;:"<>,./?'
-    # these characters will go used to go first in the password
-    NORMAL_CHARACTERS = 'abcdefghijklmnopqsrtuvqxyzABCDEFGHIJKLMNOPQSRTUVQXYZ1234567890'
-    # special characters
-    SPECIAL_CHARACTERS = '!@#$%^&*()-_+={}[]|;:"<>,./?'
+# initialize colorama
+colorama.init()
 
-    # these characters will go first in the password
-    FIRST_CHARACTER = random.choice(NORMAL_CHARACTERS)
+# define arguements for argparse
+parser = argparse.ArgumentParser(description='A password generator')
+parser.add_argument('password_length', type=int, help='Enter the the desired password length')
+args = parser.parse_args()
 
-    # get the password length
-    PASSWORD_LENGTH = input(f'{Fore.CYAN}[ {Fore.WHITE}- {Fore.CYAN}] {Fore.LIGHTBLUE_EX} Password length > {Fore.RED}')
+password_length = args.password_length
 
-    # if the user enters nothing for the length, cut the script short
-    if PASSWORD_LENGTH == '':
-        break
-    else:
-        pass
-    
-    # print [0] of the password
-    print(f"{Fore.WHITE}{FIRST_CHARACTER}", end="")
-    # generate the rest of the password
-    for _ in range(int(PASSWORD_LENGTH) - 1):
-        REST = random.choice(ALL_CHARACTERS)
-        print(REST, end="")
+# the characters that can be used in the password
+ALL_CHARACTERS = "abcdefghijklmnopqsrtuvqxyzABCDEFGHIJKLMNOPQSRTUVQXYZ1234567890!@#$%^&*()-_+={}[]|;:'<>,./?"
+# these characters will go used to go first in the password
+NORMAL_CHARACTERS = 'abcdefghijklmnopqsrtuvqxyzABCDEFGHIJKLMNOPQSRTUVQXYZ1234567890'
+# special characters
+SPECIAL_CHARACTERS = '!@#$%^&*()-_+={}[]|;:"<>,./?'
 
-    print()
-    
+# these characters will go first in the password
+FIRST_CHARACTER = random.choice(NORMAL_CHARACTERS)
+
+# print [0] of the password
+print(f'{Fore.WHITE}{FIRST_CHARACTER}', end='')
+# generate the rest of the password
+for _ in range(int(password_length) - 1):
+    REST = random.choice(ALL_CHARACTERS)
+    print(REST, end='')
+
+print()
